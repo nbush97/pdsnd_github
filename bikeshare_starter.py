769c9f8efaea19a508a48519377a4250e857b3a3
nbush97 <136000475+nbush97@users.noreply.github.com>
2025-12-05 15:13:57 -0500

    Update bikeshare_starter.py #2
    Removed empty line 20

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -17,7 +17,6 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
-
     # get user input for month (all, january, february, ... , june)
 
 
